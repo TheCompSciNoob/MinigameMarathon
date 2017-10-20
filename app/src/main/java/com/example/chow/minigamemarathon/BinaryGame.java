d3fe4049f951f521c57474b59d8c9efad9a9bc71
Alexander Berry <berryalex123@gmail.com>
2017-10-20 13:04:55 -0700

    Created BinaryGame utility class

---
+++ app/src/main/java/com/example/chow/minigamemarathon/BinaryGame.java
@@ -0,0 +1,20 @@
+package com.example.chow.minigamemarathon;
+
+import java.util.Random;
+
+/**
+ * Created by per6 on 10/20/17.
+ */
+
+public class BinaryGame {
+    public String getBinaryString(){
+        final String CHARS = "01";
+        StringBuilder sb = new StringBuilder();
+        Random rnd = new Random();
+        while(sb.length() < 50){
+            int index = (int) (rnd.nextFloat() * CHARS.length());
+            sb.append(CHARS.charAt(index));
+        }
+        return sb.toString();
+    }
+}
