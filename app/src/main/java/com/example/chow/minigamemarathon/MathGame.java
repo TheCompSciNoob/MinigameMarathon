abcfc78ef06cb08b4f2ccf2828786a7a2f8c0165
Alexander Berry <berryalex123@gmail.com>
2017-11-07 13:34:05 -0800

    Created MathGame util class and MathGameFragment

---
+++ app/src/main/java/com/example/chow/minigamemarathon/MathGame.java
@@ -0,0 +1,121 @@
+package com.example.chow.minigamemarathon;
+
+/**
+ * Created by per6 on 11/7/17.
+ */
+
+public class MathGame {
+    private int problem;
+    private int[] numbers;
+    protected int num1;
+    protected int num2;
+    protected int num3;
+    protected int num4;
+
+    public MathGame() {
+    }
+
+    public void generateProblem(){
+        num1 = (int) (Math.random() * 8 + 1);
+        num2 = (int) (Math.random() * 8 + 1);
+        num3 = (int) (Math.random() * 8 + 1);
+        num4 = (int) (Math.random() * 8 + 1);
+        numbers = new int[4];
+        numbers[0] = num1;
+        numbers[1] = num2;
+        numbers[2] = num3;
+        numbers[3] = num4;
+        double percent = Math.random();
+        if(percent < .25)
+            problem = num1 + num2;
+        else if(percent < .5)
+            problem = num1 - num2;
+        else if (percent < .75)
+            problem = num1 * num2;
+        else if (percent < 1)
+            problem = num1 / num2;
+    }
+
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if      (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if      (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+                return x;
+            }
+        }.parse();
+    }
+
+    public boolean checkAnswer(String answer) {
+        return eval(answer) == problem;
+    }
+}
