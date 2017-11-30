74bd6e273fc8582d7cc7e6dfe29b727947141484
per6 <kennethwang330842@gmail.com>
2017-11-30 13:41:03 -0800

    New Branch

---
+++ app/src/main/java/com/example/chow/minigamemarathon/Color.java
@@ -0,0 +1,38 @@
+package com.example.chow.minigamemarathon;
+
+/**
+ * Created by per6 on 11/30/17.
+ */
+
+public class Color {
+
+    private int colorNumberID;
+    private String colorStringID;
+    public void genColorID()
+    {
+        colorNumberID=(int)(Math.random()*6);
+        if (colorNumberID==0){colorStringID = "red";}
+        else if (colorNumberID==1){colorStringID = "yellow";}
+        else if (colorNumberID==2){colorStringID = "green";}
+        else if (colorNumberID==3){colorStringID = "cyan";}
+        else if (colorNumberID==4){colorStringID = "blue";}
+        else if (colorNumberID==5){colorStringID = "magenta";}
+        else{colorStringID = "NOTHING!"}
+    }
+    public int getColorNumberID() {
+        return colorNumberID;
+    }
+
+    public void setColorNumberID(int colorNumberID) {
+        this.colorNumberID = colorNumberID;
+    }
+
+    public String getColorStringID() {
+        return colorStringID;
+    }
+
+    public void setColorStringID(String colorStringID) {
+        this.colorStringID = colorStringID;
+    }
+
+}
---
+++ app/src/main/java/com/example/chow/minigamemarathon/ColorMatchFragment.java
@@ -0,0 +1,9 @@
+package com.example.chow.minigamemarathon;
+
+/**
+ * Created by per6 on 11/30/17.
+ */
+
+public class ColorMatchFragment {
+    
+}
