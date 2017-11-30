1764c3af5e47f4383499507ae503c8ea2a62c38b
per6 <kennethwang330842@gmail.com>
2017-11-30 14:07:19 -0800

    New Branch

--- app/src/main/java/com/example/chow/minigamemarathon/ColorMatchAlgorithm.java
+++ app/src/main/java/com/example/chow/minigamemarathon/ColorMatchAlgorithm.java
@@ -6,33 +6,29 @@ package com.example.chow.minigamemarathon;
 
 public class ColorMatchAlgorithm {
     private Color colorSelect1, colorSelect2, answeredColor;
-    private boolean finished=false;
-    private int questionsAnswered=0;
+    private boolean finished = false;
+    private int questionsAnswered = 0;
 
-    public void checkIfFinished()
-    {
-        if (questionsAnswered>=5)
-        {finished=true}
+    public void checkIfFinished() {
+        if (questionsAnswered >= 5) {
+            finished = true;
+        }
     }
 
-    public void genAnswer()
-    {
+    public void genAnswer() {
         answeredColor.genColorID();
     }
 
-    public void checkAnswer()
-    {
+    public void checkAnswer() {
         if (colorSelect1.getColorNumberID() == 0 && colorSelect2.getColorNumberID() == 4 || colorSelect2.getColorNumberID() == 0 && colorSelect1.getColorNumberID() == 4) {
-            if (answeredColor.getColorNumberID() == 5)
-            {
+            if (answeredColor.getColorNumberID() == 5) {
                 questionsAnswered++;
             }
 
-        }
-        else {
-            if (answeredColor.getColorNumberID() == (colorSelect1.getColorNumberID() + colorSelect2.getColorNumberID()) / 2)
-            {
+        } else {
+            if (answeredColor.getColorNumberID() == (colorSelect1.getColorNumberID() + colorSelect2.getColorNumberID()) / 2) {
                 questionsAnswered++;
             }
         }
     }
+}
